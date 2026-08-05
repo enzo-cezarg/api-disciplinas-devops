0229ea52b343907fe515fb4a9435e46634d32185
Enzo Garcia <enzoocezargarcia@gmail.com>
2026-08-05 14:14:11 -0300

    atualização dos objetos de dados

--- data/disciplinas.ts
+++ data/disciplinas.ts
@@ -7,5 +7,23 @@ export type Disciplina = {
 }
 
 export const disciplinas: Disciplina[] = [
-  { id: 1, nome: 'teste', professores: [professores.get(1)!] }
+  { id: 1, nome: 'Engenharia de Software', professores: [professores.get(1)!, professores.get(2)!] },
+  { id: 2, nome: 'Lógica e Programação de Computadores', professores: [professores.get(3)!] },
+  { id: 3, nome: 'Matemática Aplicada à Computação', professores: [professores.get(4)!, professores.get(5)!] },
+  { id: 4, nome: 'Banco de Dados', professores: [professores.get(6)!, professores.get(7)!] },
+  { id: 5, nome: 'Fundamentos de Sistemas Web', professores: [professores.get(8)!, professores.get(9)!] },
+  { id: 6, nome: 'Programação Orientada a Objetos', professores: [professores.get(10)!, professores.get(11)!] },
+  { id: 7, nome: 'Ciência de Dados e Inteligência Artificial', professores: [professores.get(12)!, professores.get(13)!] },
+  { id: 8, nome: 'Infraestrutura de Sistemas Computacionais', professores: [professores.get(14)!, professores.get(15)!] },
+  { id: 9, nome: 'Desenvolvimento de Sistemas Frontend', professores: [professores.get(16)!, professores.get(17)!] },
+  { id: 10, nome: 'Ética e Espiritualidade', professores: [professores.get(18)!] },
+  { id: 11, nome: 'Desenvolvimento de Sistemas Backend', professores: [professores.get(10)!, professores.get(11)!] },
+  { id: 12, nome: 'Empreendedorismo e Modelagem de Soluções', professores: [professores.get(19)!] },
+  { id: 13, nome: 'Segurança para Computação em Nuvem', professores: [professores.get(20)!, professores.get(21)!] },
+  { id: 14, nome: 'Desenvolvimento de Sistemas Mobile', professores: [professores.get(22)!, professores.get(13)!, professores.get(10)!] },
+  { id: 15, nome: 'Empreendedorismo e Desenvolvimento de Soluções', professores: [professores.get(23)!] },
+  { id: 16, nome: 'Comportamento Organizacional na TI', professores: [professores.get(24)!, professores.get(25)!] },
+  { id: 17, nome: 'Gerenciamento de Projetos na TI', professores: [professores.get(1)!, professores.get(26)!] },
+  { id: 18, nome: 'Disciplina Integradora de Desenvolvimento de Projetos', professores: [professores.get(1)!, professores.get(10)!, professores.get(17)!] },
+  { id: 19, nome: 'DevOps na Prática', professores: [professores.get(17)!, professores.get(27)!] }
 ];
--- data/professores.ts
+++ data/professores.ts
@@ -4,7 +4,33 @@ export type Professor = {
 }
 
 const professoresList: Professor[] = [
-  { id: 1, nome: 'fulano de tal' }
+  { id: 1, nome: 'Alessandra Costa Smolenaars Dutra' },
+  { id: 2, nome: 'Ana Paula Terra Bacelo' },
+  { id: 3, nome: 'Marcelo Cohen' },
+  { id: 4, nome: 'Lucilia Gomes Donato' },
+  { id: 5, nome: 'Daniela Rodrigues' },
+  { id: 6, nome: 'Daniel Callegari' },
+  { id: 7, nome: 'Azriel Majdenbaum' },
+  { id: 8, nome: 'Anderson Royes Terroso' },
+  { id: 9, nome: 'Andrea Aparecida Konzen' },
+  { id: 10, nome: 'Júlio Henrique Araújo Pereira Machado' },
+  { id: 11, nome: 'Bernardo Copstein' },
+  { id: 12, nome: 'Duncan Dubugras Alcoba Ruiz' },
+  { id: 13, nome: 'Michael da Costa Móra' },
+  { id: 14, nome: 'Sérgio Johann Filho' },
+  { id: 15, nome: 'Cristina Moreira Nunes' },
+  { id: 16, nome: 'Edson Ifarraguirre Moreno' },
+  { id: 17, nome: 'Cassio André Wailer Trindade' },
+  { id: 18, nome: 'Luis Evandro Hinrichsen' },
+  { id: 19, nome: 'Lucas Bonacina Roldan' },
+  { id: 20, nome: 'Avelino Francisco Zorzo' },
+  { id: 21, nome: 'Daniel Dalalana Bertoglio' },
+  { id: 22, nome: 'Alvaro Menezes' },
+  { id: 23, nome: 'Rafael Matone Chanin' },
+  { id: 24, nome: 'Dilnei Venturini' },
+  { id: 25, nome: 'Luciane Fortes' },
+  { id: 26, nome: 'Jorge Horacio Nicolas Audy' },
+  { id: 27, nome: 'Marcelo Veiga Neves' },
 ];
 
 export const professores: Map<number, Professor> = new Map(
